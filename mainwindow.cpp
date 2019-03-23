927487134ff40a9a08b7f5dbb9565e8a53286919
Keviner <hsnks100@gmail.com>
2019-03-23 21:42:28 +0900

    json editor

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "json.hpp"
+#include <iostream>
 using nlohmann::json;
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -10,41 +11,21 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
 
+
+    // QTreeWidgetItem* subItem1 = new QTreeWidgetItem;
+    // subItem1->setText(0, "? - 1");
+    // row1->addChild(subItem1);
+
+    // QTreeWidgetItem* subItem2 = new QTreeWidgetItem;
+    // subItem2->setText(0, "? - 1");
+    // row1->addChild(subItem2);
+
+    // QTreeWidgetItem* subsubItem1 = new QTreeWidgetItem;
+    // subsubItem1->setText(0, "k - 1");
+    // subItem2->addChild(subsubItem1);
+
     QTreeWidget* treewidget = ui->treeWidget;
-    QTreeWidgetItem* header = new QTreeWidgetItem;
-    header->setText(0, "name");
-    header->setText(1, "age");
-    header->setText(2, "etc");
-    treewidget->setHeaderItem(header);
-
-    QTreeWidgetItem* row1 = new QTreeWidgetItem(treewidget);
-    row1->setText(0, "col 1");
-    row1->setText(1, "col 2");
-    row1->setText(2, "col 3");
-
-    QTreeWidgetItem* row2 = new QTreeWidgetItem(treewidget);
-    // treewidget->addTopLevelItem(row2);
-    row2->setText(0, "?");
-    row2->setText(1, "??");
-    row2->setText(2, "???");
-
-    QTreeWidgetItem* subItem1 = new QTreeWidgetItem;
-    subItem1->setText(0, "? - 1");
-    subItem1->setText(1, "? - 2");
-    subItem1->setText(2, "? - 3");
-    row2->addChild(subItem1);
-
-    QTreeWidgetItem* subItem2 = new QTreeWidgetItem;
-    subItem2->setText(0, "? - 1");
-    subItem2->setText(1, "? - 2");
-    row2->addChild(subItem2);
-
-    QTreeWidgetItem* subsubItem1 = new QTreeWidgetItem;
-    subsubItem1->setText(0, "k - 1");
-    subsubItem1->setText(1, "k - 2");
-    subItem2->addChild(subsubItem1);
-
-    // treewidget->resize(500, 500);
+    treewidget->resize(500, 500);
     // treewidget->setSortingEnabled(true);
     // treewidget->show();
 
@@ -57,17 +38,99 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_plainTextEdit_textChanged()
 {
+
     auto t = ui->plainTextEdit->toPlainText();
     json j;
 
     try {
         json j3 = json::parse(t.toStdString());
-        travelJson(j3);
+        QTreeWidget* treewidget = ui->treeWidget;
+        treewidget->clear();
+        QTreeWidgetItem* header = new QTreeWidgetItem;
+        header->setText(0, "name");
+        treewidget->setHeaderItem(header);
+
+        QTreeWidgetItem* row1 = new QTreeWidgetItem(treewidget);
+        row1->setText(0, "JSON");
+
+
+        travelJson("JSON", j3, row1);
+        treewidget->expandAll();
+        // travelJson(j3);
     } catch(json::parse_error) {
         printf("failed\n");
     }
 }
 
-void MainWindow::travelJson(const nlohmann::json& j) {
+/*
+{"a":1, "b":2, "c":{"d1":1, "d2":2}, "darr":[100,200,300,{"d1":1, "d2":2}]}
+*/
+
+void MainWindow::travelJson(const std::string& keyName, const nlohmann::json& j, QTreeWidgetItem* qtree) {
     // somethings.
+    if(j.is_object()) {
+        // std::string t = i.key();
+        QTreeWidgetItem* row1 = new QTreeWidgetItem;
+        std::string key = getTypeString(j) + keyName;
+        row1->setText(0, QString(key.c_str()));
+        qtree->addChild(row1);
+
+        for (auto i = j.begin(); i != j.end(); ++i) {
+            // std::cout << *it << '\n';
+            travelJson(i.key(), *i, row1);
+        }
+    }
+    else if(j.is_array()) {
+        QTreeWidgetItem* row1 = new QTreeWidgetItem;
+        std::string key = getTypeString(j) + keyName;
+        row1->setText(0, QString(key.c_str()));
+        qtree->addChild(row1);
+        for(auto& i: j.items()) {
+            // std::string t = i.key();
+            // std::cout << t << ":";
+            travelJson(i.key(), i.value(), row1);
+        }
+
+    }
+    else if(j.is_primitive()) {
+        std::string v;
+        v = keyName + ":" + j.dump();
+        QTreeWidgetItem* row1 = new QTreeWidgetItem;
+        row1->setText(0, QString(v.c_str()));
+        qtree->addChild(row1);
+        std::cout << v << std::endl;
+    }
+    // printf("%d\n", __LINE__);
+    // if(j.is_object()) {
+    //     for(auto& i: j.items()) {
+    //         printf("%s\n", i.key().c_str());
+    //         if(i.value().is_object()) {
+    //             QTreeWidgetItem* row1 = new QTreeWidgetItem;
+    //             std::string key = i.key();
+    //             row1->setText(0, QString(key.c_str()));
+    //             printf("qtree->%s\n", key.c_str());
+    //             qtree->addChild(row1);
+
+    //             travelJson(i.value(), row1);
+    //         }
+    //         else if(i.value().is_primitive()) {
+    //             printf("%d\n", __LINE__);
+    //             QTreeWidgetItem* row1 = new QTreeWidgetItem;
+    //             printf("%d\n", __LINE__);
+    //             std::string value = "";
+    //             if(i.value().is_string()) {
+    //                 value = i.value().get<std::string>();
+    //             }
+    //             else if(i.value().is_number()) {
+    //                 value = i.value().get<double>();
+    //             }
+    //             printf("%d\n", __LINE__);
+    //             row1->setText(0, QString(value.c_str()));
+    //             printf("%d\n", __LINE__);
+    //             qtree->addChild(row1);
+    //             printf("qtree->%s\n", value.c_str());
+
+    //         }
+    //     }
+    // }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <iostream>
 #include "json.hpp"
 
 namespace Ui {
 class MainWindow;
 }
+#include "ui_mainwindow.h"
 
 class MainWindow : public QMainWindow
 {
@@ -21,7 +23,29 @@ private slots:
 
 public:
     Ui::MainWindow *ui;
-    void travelJson(const nlohmann::json& j);
+    void travelJson(const std::string& keyName, const nlohmann::json& j, QTreeWidgetItem* qtree);
+    std::string getTypeString(const nlohmann::json& j) {
+        std::string ret;
+        if(j.is_array()) {
+            ret =  "[]";
+        }
+        else if(j.is_object()) {
+            ret =  "{}";
+        }
+        else {
+            ret =  "nnn";
+        }
+        std::cout << j.dump() << "=>" << ret << std::endl;
+        return ret;
+    }
+    void showEvent( QShowEvent* event ) {
+        std::string jsonText = R"(
+{"a":1, "b":2, "c":{"d1":1, "d2":2}, "darr":[100,200,300,{"d1":1, "d2":2}]}
+)";
+        ui->plainTextEdit->setPlainText(jsonText.c_str());
+        on_plainTextEdit_textChanged();
+    }
+
 
 };
 
